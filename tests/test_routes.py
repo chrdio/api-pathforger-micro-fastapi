0e7ac8552039c4c08cc1535ff99ca730db179924
Arthur Ryzhak <ryzhakar@gmail.com>
2022-08-29 20:18:32 +0300

    Add true failing tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,12 +12,22 @@ from pathforger import getProgression, TEST_GRAPH, NODES, GRAPH_DICT
 TEST_APP = TestClient(APP, raise_server_exceptions=False)
 
 @given(body=st.builds(ProgressionRequest), length=st.integers(min_value=2, max_value=4))
-def test_generation(body, length):
+def test_generation_200(body, length):
     payload = body.json()
     response = TEST_APP.post(f'/generate/{str(length)}', payload)
     assert response.status_code == 200
 
-@settings(suppress_health_check=[HealthCheck(9)])
+@given(length=st.integers(min_value=2, max_value=4))
+def test_generation_200_no_body(length):
+    payload = "{}"
+    response = TEST_APP.post(f'/generate/{str(length)}', payload)
+    assert response.status_code == 200
+
+def test_generation_422():
+    payload = '{"graph": 4}'
+    response = TEST_APP.post(f'/generate/{str(3)}', payload)
+    assert response.status_code == 422
+
 @given(
     index=st.integers(
         min_value=0,
@@ -31,11 +41,40 @@ def test_generation(body, length):
         ]
         )
     )
-def test_amendment(index, graph):
+def test_amendment_200(index, graph):
     progression = getProgression(4, graph, NODES)
     payload = progression.json()
     response = TEST_APP.post(f'/amend/{str(index)}', payload)
     assert response.status_code == 200
+
+@given(
+    index=st.integers(
+        min_value=4,
+        ),
+    graph=st.sampled_from(
+        [
+            GRAPH_DICT["major_graph"],
+            GRAPH_DICT["minor_graph"],
+            GRAPH_DICT["master_graph"],
+        ]
+        )
+    )
+def test_amendment_500(index, graph):
+    progression = getProgression(4, graph, NODES)
+    payload = progression.json()
+    response = TEST_APP.post(f'/amend/{str(index)}', payload)
+    assert response.status_code == 500
+
+@given(
+    index=st.integers(
+        min_value=0,
+        max_value=3
+        )
+    )
+def test_amendment_422(index):
+    payload = '{"graph": "test_graph"}'
+    response = TEST_APP.post(f'/amend/{str(index)}', payload)
+    assert response.status_code == 422
 
 
 
